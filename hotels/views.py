d80f17d68aec23dc89568434c52035c131838bf3
DimiterM <dimiter_m@yahoo.com>
2014-05-27 00:00:08 +0300

    Search form front-end

--- hotels/views.py
+++ hotels/views.py
@@ -1,13 +1,17 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 
-from hotels.models import Hotel
+from hotels.models import Hotel, Tag
+
+from hotels.forms import SearchHotel
+
 
 def index(request, page_num=1):
 	page_num = int(page_num)
 	previous = page_num - 1
 	next = page_num + 1
 	hotels_list = Hotel.objects.all()[((page_num-1) * 10) : (page_num * 10 - 1)]
+	tags_list = Tag.objects.all()
 	return render(request, "index.html", locals())
 
 
@@ -22,3 +26,16 @@ def reserve(request, hotel_id):
 
 def login(request):
     return render(request, "login.html", locals())
+    
+
+def search(request):
+    data = request.POST if request.POST else None
+    form = SearchHotel(data)
+    
+#    if request.method == 'POST':
+#        if form.is_valid():
+#            form.save()
+#            return redirect('index')
+    
+    return render(request, "index.html", locals())
+
